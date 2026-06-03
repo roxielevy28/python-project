3e64b058e1bd056c3e1b18c07a24d005b74f5ad0
Roxanne Levy <roxielevy28@yahoo.com>
2026-06-03 19:58:51 -0400

    save changes

--- scrapetest.py
+++ scrapetest.py
@@ -7,14 +7,39 @@ print(soup)
 product = soup.find(id="default")
 print (product)
 
-items = product.find_all(class_= 'container-fluid page')
+items = product.find_all(class_= 'col-sm-6 product_main')
 print(items)
 
-print(items[0].find(class_= 'product_pod').get_text())
-print(items[0].find(class_= 'star-rating Five').get_text())
+print(items[0].find(class_= 'instock availability')
+print(items[0].find(class_= 'star-rating Five')
 
-product_page_url = [items[0].find(class_= 'product_pod').get_text()]
-review_rating = [items[0].find(class_= 'star-rating Five').get_text()]
+quantity_available = [items[0].find(class_= 'instock availability')
+review_rating = [items[0].find(class_= 'star-rating Five')
+                 
 
-print(product_page_url)
+items_2 = product.find_all(class_= 'table table-striped')
+
+print(items[2].find(class_= '')
+      
+universal_ product_code (upc) = items_2[0].find_
+price_including_tax = items_2[0].find_
+price_excluding_tax
+
+print(quantity_available)
 print(review_rating)
+
+
+Book_Report = pd.DataFrame(
+    {
+    "product_page_url",
+    "universal_product_code",
+    "book_title",
+    "price_including_tax",
+    "price_excluding_tax",
+    "quantity_available",
+    "product_description",
+    "category",
+    "review_rating",
+    "image_url"
+    })
+    
