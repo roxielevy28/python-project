947f5e7e5220692a86d87922bd71c33747c9eae3
roxielevy28 <roxielevy28@yahoo.com>
2026-06-09 18:47:43 -0400

    save

--- Phase2_Young.Adult.category.py
+++ Phase2_Young.Adult.category.py
@@ -2,5 +2,6 @@ import pandas as pd
 import request
 from bs4 import BeautifulSoup
 
-page = requests.get('https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html')
+catergory= requests.get('https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html')
 soup = BeautifulSoup (page.content, 'html.parser')
+
