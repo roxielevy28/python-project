49f8d48f9552610fa62867cff3df3bd6ae6d33d1
roxielevy28 <roxielevy28@yahoo.com>
2026-06-10 19:45:05 -0400

    updates for today

--- Phase2_Young.Adult.category.py
+++ Phase2_Young.Adult.category.py
@@ -1,13 +1,26 @@
 from urllib.parse import urljoin
+
 import pandas as pd 
-import request
+import requests
 from bs4 import BeautifulSoup
 
 catergory= requests.get('https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html')
-soup = BeautifulSoup (page.content, 'html.parser')
+soup = BeautifulSoup (catergory.content, 'html.parser')
 
-books_on_page= product.find_all(class_= 'product_pod')
+product = soup.find(id="default")
 
-raw_link=(books_on_page[0].find('h3').find('a')['href'])
-Actual_link= urljoin("https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html", raw_link)
-print(Actual_link)
+books_on_page= soup.find_all(class_= 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
+
+raw_book_link=(books_on_page[0].find('h3').find('a')['href'])
+Actual_book_link= urljoin("https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html", raw_book_link)
+print(Actual_book_link) # i was trying to get the link for all the books on the page but i was only able to get the link for the first book. I was trying to loop through the books_on_page list but i was not able to get it to work.
+
+in_the_event_of_other_pages= soup.find(class_='next')
+for i in range(1, 3): # this catergory has 3 pages I was trying to get the link for the remaining 2 pages
+    url="https://books.toscrape.com/catalogue/category/books/young-adult_21/page-"+str(i)+".html"
+    next_page_links= in_the_event_of_other_pages.find('a')['href']
+    actual_next_page_links= urljoin("https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html", next_page_links)
+
+print(actual_next_page_links)
+
+
