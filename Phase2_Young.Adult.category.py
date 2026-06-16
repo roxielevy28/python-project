9fe2335ca44923bf474f00866565ae812b7c8c7a
roxielevy28 <roxielevy28@yahoo.com>
2026-06-16 00:01:40 +0000

    that's ggod for now

--- Phase2_Young.Adult.category.py
+++ Phase2_Young.Adult.category.py
@@ -3,9 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 
 category_url = "https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html"
-page = requests.get(category_url)
-# print(category_url) # this works, i get the url
+url_for_all_books= []
 
+page = requests.get(category_url)
 soup = BeautifulSoup(page.text,'html.parser')
 
 # link from each book on the page
@@ -14,20 +14,21 @@ books_on_page= soup.find_all(class_= 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
 
 for book_element in books_on_page:
     link = book_element.find('h3').find('a')['href']
-    full_url = urljoin(category_url, link)
-    # print(full_url) 
-    # this works now. i got all the links on the first page
+    complete_url = urljoin(category_url, link)
+    # print(complete_url) 
+    url_for_all_books.append(complete_url)
+     # this works now. i got all the links on the first page
 
 next_button = soup.find(class_='next')
 if next_button:
         next_page = next_button.find("a")["href"]
-        additional_url = urljoin(category_url, next_page)
+        additional_page_url = urljoin(category_url, next_page)
 
 else:
-    additional_url = None
+    additional_page_url = None
 
 all_books = []
-for url in all_book_urls:
+for url in url_for_all_books:
     book_data = scrape_one_book(url)
     all_books.append(book_data)
 
