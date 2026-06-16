150c82dc3b4210dff8b9c1f3992cb14e9d2aaa28
roxielevy28 <roxielevy28@yahoo.com>
2026-06-16 00:44:48 +0000

    good for tonight

--- Phase2_Young.Adult.category.py
+++ Phase2_Young.Adult.category.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 category_url = "https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html"
-url_for_all_books= []
+url_for_all_books_in_category= []
 
 page = requests.get(category_url)
 soup = BeautifulSoup(page.text,'html.parser')
@@ -15,8 +15,8 @@ books_on_page= soup.find_all(class_= 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
 for book_element in books_on_page:
     link = book_element.find('h3').find('a')['href']
     complete_url = urljoin(category_url, link)
-    # print(complete_url) 
-    url_for_all_books.append(complete_url)
+    print(complete_url) 
+    url_for_all_books_in_category.append(complete_url)
      # this works now. i got all the links on the first page
 
 next_button = soup.find(class_='next')
@@ -28,8 +28,8 @@ else:
     additional_page_url = None
 
 all_books = []
-for url in url_for_all_books:
-    book_data = scrape_one_book(url)
+for url in url_for_all_books_in_category:
+    book_data = scrape_one_book(url) # this is where i will call the function that i created in scrapetest.py, however i understand this wont work right now because i havent connected it yet.
     all_books.append(book_data)
 
 master_report = pd.DataFrame(all_books)
