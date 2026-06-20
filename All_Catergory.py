11cee58a9996ca741eb3081a4e9118d002afa389
roxielevy28 <roxielevy28@yahoo.com>
2026-06-20 16:36:22 -0400

    save

--- All_Catergory.py
+++ All_Catergory.py
@@ -2,3 +2,12 @@ from urllib.parse import urljoin
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+
+home_url= requests.get('https://books.toscrape.com/index.html')
+page = requests.get(home_url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+category_links= []
+page_content= soup.find(id="default")
+category_list = page.find_all(class_= 'nav nav-list')
+print(category_list)
